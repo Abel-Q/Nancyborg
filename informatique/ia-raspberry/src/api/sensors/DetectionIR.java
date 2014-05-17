78c2ef745d1688193ac60cc5d16380cecbdbca7f
Francois Prugniel <francois.prugniel@telecomnancy.net>
2014-05-17 12:04:35 +0200

    c'est une api bordel, faut que ce soit paramétrable

--- informatique/ia-raspberry/src/api/sensors/DetectionIR.java
+++ informatique/ia-raspberry/src/api/sensors/DetectionIR.java
@@ -1,5 +1,7 @@
 package api.sensors;
 
+import ia.nancyborg2014.Ia;
+
 import java.util.concurrent.Callable;
 
 import navigation.Point;
@@ -8,9 +10,9 @@ import api.ax12.AX12Base;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.PinPullResistance;
 import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.io.gpio.trigger.GpioCallbackTrigger;
 
 //TODO: - mettre a jour l'angle de notre robot
@@ -18,26 +20,23 @@ import com.pi4j.io.gpio.trigger.GpioCallbackTrigger;
 // - metre a jour les angles des capteurs et de la rotation
 
 public class DetectionIR extends Thread {
-	private final float[] angles = {-15.0f, 0.0f, 15.0f};
-	private Point nous, adversaire;
-	private float angle;
+	private float[] anglesCapteurs;
 	private AX12Base ax12;
+	private Ia ia; // Histoire de signaler à l'IA qu'il y a un adversaire
 
 	public static double DISTANCE = 30.0;
 
-	public DetectionIR(Point nous, float angle, Point adversaire, AX12Base ax12){
-		this.nous = nous;
-		this.angle = angle;
-		this.adversaire = adversaire;
+	public DetectionIR(float[] anglesCapteurs, AX12Base ax12, Pin pinCapteur1, Pin pinCapteur2, Pin pinCapteur3){
+		this.anglesCapteurs = anglesCapteurs;
 		this.ax12 = ax12;
 
 		// create gpio controller
         final GpioController gpio = GpioFactory.getInstance();
 
         // provision gpio pin #02 as an input pin with its internal pull down resistor enabled
-        final GpioPinDigitalInput capteur0 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
-        final GpioPinDigitalInput capteur1 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_03, PinPullResistance.PULL_DOWN);
-        final GpioPinDigitalInput capteur2 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_04, PinPullResistance.PULL_DOWN);
+        final GpioPinDigitalInput capteur0 = gpio.provisionDigitalInputPin(pinCapteur1, PinPullResistance.PULL_DOWN);
+        final GpioPinDigitalInput capteur1 = gpio.provisionDigitalInputPin(pinCapteur2, PinPullResistance.PULL_DOWN);
+        final GpioPinDigitalInput capteur2 = gpio.provisionDigitalInputPin(pinCapteur3, PinPullResistance.PULL_DOWN);
         
 
         //add trigger event
@@ -66,14 +65,18 @@ public class DetectionIR extends Thread {
 
 	public void run(){
 		while(true){
+			// TODO timer pour la rotation
 			this.ax12.setGoalPosition(0.0f, true);
 			this.ax12.setGoalPosition(45.0f, true);
 		}
 	}
 
-	public void detected(int capteur){
-		float angle = this.ax12.getPresentPosition() + this.angles[capteur] - this.angle;
-		this.adversaire.setX(this.nous.getX() + (int) (Math.cos(Math.PI * angle) * DetectionIR.DISTANCE));
-		this.adversaire.setY(this.nous.getY() + (int) (Math.sin(Math.PI * angle) * DetectionIR.DISTANCE));
+	public void detected(int capteur) {
+		Point nous = ia.getPosition();
+		// TODO ça mélange des degrés et des radians, à fixer !!
+		float angle = this.ax12.getPresentPosition() + this.anglesCapteurs[capteur] - nous.getCap();
+		int x = (nous.getX() + (int) (Math.cos(Math.PI * angle) * DetectionIR.DISTANCE));
+		int y = (nous.getY() + (int) (Math.sin(Math.PI * angle) * DetectionIR.DISTANCE));
+		ia.detectionAdversaire(new Point(x, y));
 	}
 }
